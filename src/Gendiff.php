c9a43bcb46e07c0446fbba43b43abcd5b79bce34
Alexsey-VR <vityaz.iss@yandex.ru>
2025-06-08 18:16:12 +0000

    added print help

---
+++ src/Gendiff.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+//require_once __DIR__ . '/../vendor/autoload.php';
+
+use Docopt;
+
+function runGendiff()
+{
+    $doc = <<<'DOCOPT'
+    gendiff -h
+
+    Generate diff
+
+    Usage:
+      gendiff (-h|--help)
+      gendiff (-v|--version)
+
+    Options:
+      -h --help                  Show this screen
+      -v --version               Show version
+
+    DOCOPT;
+
+    $result = Docopt::handle($doc, array('version' => '1.0.6'));
+
+    foreach ($result as $k => $v) {
+        print_r($k . ': ' . json_encode($v) . PHP_EOL);
+    }
+}
