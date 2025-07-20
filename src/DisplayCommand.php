a61534f2f8378fd89497caf7a117e8ee0c8dbd3c
Alexsey-VR <vityaz.iss@yandex.ru>
2025-07-20 08:55:29 +0000

    update DisplayCommand

--- src/DisplayCommand.php
+++ src/DisplayCommand.php
@@ -20,41 +20,44 @@ class DisplayCommand implements CommandInterface
 
     public function execute(CommandInterface $command = null): CommandInterface
     {
-        $filesContent = $command->getFilesContent();
-        $keys = array_keys($filesContent);
-        $file1Content = $filesContent[$keys[0]];
-        $file2Content = $filesContent[$keys[1]];
+        if (!is_null($command)) {
+            $filesContent = $command->getFilesContent();
 
-        $this->filesContent[] = "file1.json content:\n";
-        $this->filesContent[] = array_reduce(
-            $file1Content,
-            [$this, 'constructContent'],
-            "{"
-        ) . "\r}\n";
+            $keys = array_keys($filesContent);
+            $file1Content = $filesContent[$keys[0]];
+            $file2Content = $filesContent[$keys[1]];
 
-        $this->filesContent[] = "file2.json content:\n";
-        $this->filesContent[] = array_reduce(
-            $file2Content,
-            [$this, 'constructContent'],
-            "{"
-        ) . "\n}\n";
+            $this->filesContent[] = "file1.json content:\n";
+            $this->filesContent[] = array_reduce(
+                $file1Content,
+                [$this, 'constructContent'],
+                "{"
+            ) . "\r}\n";
 
-        $file1Keys = array_keys($file1Content);
-        $this->filesDiffContent = array_map(
-            function ($file1Key) use ($file1Content, $file2Content) {
-                if (array_key_exists($file1Key, $file2Content)) {
-                    if (!strcmp($file1Content[$file1Key], $file2Content[$file1Key])) {
-                        return "    " . $file1Content[$file1Key] . "\n";
+            $this->filesContent[] = "file2.json content:\n";
+            $this->filesContent[] = array_reduce(
+                $file2Content,
+                [$this, 'constructContent'],
+                "{"
+            ) . "\n}\n";
+
+            $file1Keys = array_keys($file1Content);
+            $this->filesDiffContent = array_map(
+                function ($file1Key) use ($file1Content, $file2Content) {
+                    if (array_key_exists($file1Key, $file2Content)) {
+                        if (!strcmp($file1Content[$file1Key], $file2Content[$file1Key])) {
+                            return "    " . $file1Content[$file1Key] . "\n";
+                        } else {
+                            return "  - " . $file1Content[$file1Key] . "\n" .
+                                "  + " . $file2Content[$file1Key] . "\n";
+                        }
                     } else {
-                        return "  - " . $file1Content[$file1Key] . "\n" .
-                               "  + " . $file2Content[$file1Key] . "\n";
+                        return "  - " . $file1Content[$file1Key] . "\n";
                     }
-                } else {
-                    return "  - " . $file1Content[$file1Key] . "\n";
-                }
-            },
-            $file1Keys
-        );
+                },
+                $file1Keys
+            );
+        }
         return $this;
     }
 
