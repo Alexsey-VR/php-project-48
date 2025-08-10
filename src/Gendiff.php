c7566a73908a25ac217aec69f1dc8e37f4283386
Alexsey-VR <vityaz.iss@yandex.ru>
2025-08-10 14:24:29 +0700

    update constructor in Gendiff

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -4,6 +4,26 @@ namespace Differ;
 
 function runGendiff(): void
 {
-    $consoleApp = new ConsoleApp();
+    $docopt = <<<'DOCOPT'
+    gendiff -h
+
+    Generate diff
+
+    Usage:
+        gendiff (-h|--help)
+        gendiff (-v|--version)
+        gendiff [Options]... FILE1 FILE2
+
+    Options:
+        -h --help                  Show this screen
+        -v --version               Show version
+        --format <fmt>             Report format [default: stylish]
+
+    DOCOPT;
+
+    $commandFactory = new CommandFactory($docopt);
+    $fileReader = new FileReader();
+
+    $consoleApp = new ConsoleApp($commandFactory, $fileReader);
     $consoleApp->run();
 }
