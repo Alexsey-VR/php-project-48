8702c0ae67c454fae6bf68f50dbf5a234694fd67
Alexsey-VR <vityaz.iss@yandex.ru>
2025-10-06 03:59:57 +0000

    added annotation to docopt double

--- src/DocoptDouble.php
+++ src/DocoptDouble.php
@@ -4,19 +4,25 @@ namespace Differ;
 
 class DocoptDouble
 {
+    /**
+     * @var array<string,string>
+     */
     public array $args;
 
     public function __construct(string $format = "stylish")
     {
         $this->args = [
-            "--help" => null,
-            "--version" => null,
+            "--help" => "",
+            "--version" => "",
             "--format" => $format,
             "FILE1" => __DIR__ . "/../fixtures/file1.json",
             "FILE2" => __DIR__ . "/../fixtures/file2.json"
         ];
     }
 
+    /**
+     * @return mixed
+     */
     public function handle()
     {
         return $this;
