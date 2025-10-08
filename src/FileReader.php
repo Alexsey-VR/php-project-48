4c8433e24f3887adcaa318a95082694b890bd75a
Alexsey-VR <vityaz.iss@yandex.ru>
2025-10-08 16:19:38 +0000

    added annotation to file reader

--- src/FileReader.php
+++ src/FileReader.php
@@ -7,46 +7,41 @@ use Symfony\Component\Yaml\Yaml;
 class FileReader implements FileReaderInterface
 {
     private const MAX_FILE_SIZE = 4096;
-    private string $fileFormat;
 
-    private function normalizeFilename(string $filename)
+    private function normalizeFilename(string $filename): string
     {
         return strtolower($filename);
     }
 
-    public function __construct(string $fileFormat = 'json')
+    /**
+     * @return array<mixed,mixed>
+     */
+    public function readFile(string $filename): array
     {
-        $this->fileFormat = $fileFormat;
-    }
-
-    public function readFile(string $filename, bool $isArray = true): ?array
-    {
-        $fileContentArray = null;
-        if (file_exists($filename)) {
-            $fileNameParts = explode(".", $this->normalizeFilename($filename));
-            $fileFormat = end($fileNameParts);
-            if ($fileFormat === 'json') {
-                $handle = fopen($filename, "r");
-                $jsonVariables = json_decode(
-                    fread($handle, self::MAX_FILE_SIZE),
-                    $isArray
-                );
-                fclose($handle);
-                $type = gettype($jsonVariables);
-                if ($type === 'object') {
-                    $fileContentArray = get_object_vars($jsonVariables);
-                } elseif ($type === 'array') {
-                    $fileContentArray = $jsonVariables;
-                }
-            } elseif ($fileFormat === 'yaml' || $fileFormat === 'yml') {
-                $fileContentArray = Yaml::parseFile($filename);
-            } else {
-                throw new DifferException("unknown files format: use json, yaml (yml) enstead\n");
-            }
-
-            return $fileContentArray;
+        $fileContentArray = [];
+        $fileNameParts = explode(".", $this->normalizeFilename($filename));
+        $fileFormat = end($fileNameParts);
+        if ($fileFormat === 'json') {
+            $handle = fopen($filename, "r");
+            $fileData = fread(
+                ($handle !== false) ? $handle : throw new DifferException("internal error: can't open file\n"),
+                self::MAX_FILE_SIZE
+            );
+            $jsonVariables = json_decode(
+                ($fileData !== false) ? $fileData : "",
+                flags: JSON_OBJECT_AS_ARRAY
+            );
+            fclose($handle);
+            $fileContentArray = is_array($jsonVariables) ? $jsonVariables
+            : throw new DifferException("internal error: json file can't be parsed\n");
+        } elseif ($fileFormat === 'yaml' || $fileFormat === 'yml') {
+            $yamlVariables = Yaml::parseFile($filename);
+            $fileContentArray = is_array($yamlVariables) ? $yamlVariables
+            : throw new DifferException("internal error: yaml file can't be parsed\n");
         } else {
-            return null;
+            throw new DifferException("unknown files format: use json, yaml (yml) enstead\n");
         }
+
+        return $fileContentArray;
     }
 }
