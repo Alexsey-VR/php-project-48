248821e61f5221778db7690c01e1a27bbb69717e
Alexsey-VR <vityaz.iss@yandex.ru>
2025-10-09 03:01:44 +0000

    updated command line parser interface

--- src/CommandLineParserInterface.php
+++ src/CommandLineParserInterface.php
@@ -5,14 +5,17 @@ namespace Differ;
 interface CommandLineParserInterface
 {
     public function execute(CommandLineParserInterface $command): CommandLineParserInterface;
+    
     /**
      * @param array<string,string> $fileNames
      */
     public function setFileNames(array $fileNames): CommandLineParserInterface;
+    
     /**
      * @return array<string,string>
      */
     public function getFileNames(): array;
+    
     public function setFormat(string $format): CommandLineParserInterface;
     public function getFormat(): string;
 }
